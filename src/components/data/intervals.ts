893a280b7629eb178f0ddbf231df95603216d759
CLICKNEXT\Saiswan.w <sai@clicknext.com>
2025-10-21 14:20:58 +0700

    format code

--- src/components/data/intervals.ts
+++ src/components/data/intervals.ts
@@ -2,136 +2,126 @@ import { Tonality } from '@data/constants';
 import { majorKeyToNumber, minorKeyToNumber } from "@data/constants";
 
 interface Intervals {
-    [key: string]: number[];
+  [key: string]: number[];
 }
-
-/*
-Intervals for "C" Major Key
-1. roots
-2. major seconds
-3. major thirds
-4. perfect fourths
-5. perfect fifths
-6. major sixths
-7. major sevenths
-*/
 
 const roots: Intervals = {
-    e: [-4,8,20],
-    B: [-11,1,13],
-    G: [-7,5,17],
-    D: [-2,10,22],
-    A: [-9,3,15],
-    E: [-4,8,20]
-}
+  e: [-4, 8, 20],
+  B: [-11, 1, 13],
+  G: [-7, 5, 17],
+  D: [-2, 10, 22],
+  A: [-9, 3, 15],
+  E: [-4, 8, 20],
+};
 
 const minorSeconds: Intervals = {
-    e: [-3, 9, 21], 
-    B: [-10, 2, 14], 
-    G: [-6, 6, 18], 
-    D: [-1, 11, 23], 
-    A: [-8, 4, 16],
-    E: [-3, 9, 21]
+  e: [-3, 9, 21], 
+  B: [-10, 2, 14], 
+  G: [-6, 6, 18], 
+  D: [-1, 11, 23], 
+  A: [-8, 4, 16],
+  E: [-3, 9, 21]
 }
 
 const seconds: Intervals = {
-    e: [-2,10,22],
-    B: [-9,3,15],
-    G: [-5,7,19],
-    D: [0,12,24],
-    A: [-7,5,17],
-    E: [-2,10,22]
-}
+  e: [-2, 10, 22],
+  B: [-9, 3, 15],
+  G: [-5, 7, 19],
+  D: [0, 12, 24],
+  A: [-7, 5, 17],
+  E: [-2, 10, 22],
+};
 
 const minorThirds: Intervals = {
-    e: [-1, 11, 23], 
-    B: [-8, 4, 16], 
-    G: [-4, 8, 20], 
-    D: [1, 13, 25], 
-    A: [-6, 6, 18],
-    E: [-1, 11, 23]
+  e: [-1, 11, 23], 
+  B: [-8, 4, 16], 
+  G: [-4, 8, 20], 
+  D: [1, 13, 25], 
+  A: [-6, 6, 18],
+  E: [-1, 11, 23]
+
 }
 
 const thirds: Intervals = {
-    e: [0,12,24],
-    B: [-7,5,17],
-    G: [-3,9,21],
-    D: [-10,2,14],
-    A: [-5,7,19],
-    E: [0,12,24]
-}
+  e: [0, 12, 24],
+  B: [-19, -7, 5, 17],
+  G: [-3, 9, 21],
+  D: [-10, 2, 14, 26],
+  A: [-5, 7, 19],
+  E: [0, 12, 24]
+};
 
 const fourths: Intervals = {
-    e: [1,13,25],  
-    B: [-18,-6,6,18],  
-    G: [-2,10,22], 
-    D: [-9,3,15,27],  
-    A: [-4,8,20],  
-    E: [1,13,25]  
-}
+  e: [1, 13, 25],
+  B: [-18, -6, 6, 18],
+  G: [-2, 10, 22],
+  D: [-9, 3, 15, 27],
+  A: [-4, 8, 20],
+  E: [1, 13, 25],
+};
 
 const tritones: Intervals = {
-    e: [2, 14, 26], 
-    B: [-17, -5, 7, 19], 
-    G: [-1, 11, 23], 
-    D: [-8, 4, 16, 28], 
-    A: [-3, 9, 21],
-    E: [2, 14, 26]
-}
+  e: [2, 14, 26],
+  B: [-17, -5, 7, 19],
+  G: [-1, 11, 23],
+  D: [-8, 4, 16, 28],
+  A: [-3, 9, 21],
+  E: [2, 14, 26],
+};
 
 const fifths: Intervals = {
-    e: [-9,3,15],
-    B: [-4,8,20],
-    G: [0,12,24],
-    D: [-7,5,17],
-    A: [-2,10,22],
-    E: [-9,3,15]
-}
+  e: [-9, 3, 15],
+  B: [-4, 8, 20],
+  G: [0, 12, 24],
+  D: [-7, 5, 17],
+  A: [-2, 10, 22],
+  E: [-9, 3, 15],
+};
 
 const minorSixths: Intervals = {
-    e: [-8, 4, 16], 
-    B: [-3, 9, 21], 
-    G: [1, 13, 25], 
-    D: [-6, 6, 18], 
-    A: [-1, 11, 23],
-    E: [-8, 4, 16]
-}
+  e: [-8, 4, 16],
+  B: [-3, 9, 21],
+  G: [1, 13, 25],
+  D: [-6, 6, 18],
+  A: [-1, 11, 23],
+  E: [-8, 4, 16],
+};
 
 const sixths: Intervals = {
-    e: [-7,5,17,29],  
-    B: [-2,10,22], 
-    G: [-10,2,14,26],  
-    D: [-5,7,19],
-    A: [0,12,24],  
-    E: [-7,5,17,29]
-}
+  e: [-7, 5, 17, 29],
+  B: [-2, 10, 22],
+  G: [-10, 2, 14, 26],
+  D: [-5, 7, 19],
+  A: [0, 12, 24],
+  E: [-7, 5, 17, 29],
+};
 
 const minorSevenths: Intervals = {
-    e: [-6, 6, 18, 30], 
-    B: [-1, 11, 23], 
-    G: [-9, 3, 15, 27], 
-    D: [-4, 8, 20], 
-    A: [1, 13, 25], 
-    E: [-6, 6, 18, 30]
-}
+  e: [-6, 6, 18, 30],
+  B: [-1, 11, 23],
+  G: [-9, 3, 15, 27],
+  D: [-4, 8, 20],
+  A: [1, 13, 25],
+  E: [-6, 6, 18, 30],
+};
 
 const sevenths: Intervals = {
-    e: [-5,7,19,31],  
-    B: [0,12,24],  
-    G: [-8,4,16,28],  
-    D: [-3,9,21],
-    A: [-10,2,14,26],  
-    E: [-5,7,19,31] 
-}
+  e: [-5, 7, 19, 31],
+  B: [0, 12, 24],
+  G: [-8, 4, 16, 28],
+  D: [-3, 9, 21],
+  A: [-10, 2, 14, 26],
+  E: [-5, 7, 19, 31],
+};
 
 const blues: Intervals = {
-    e: [-1,11,23],
-    B: [-8,4,16],
-    G: [-4,8,20],
-    D: [-11,1,13],
-    A: [-6,6,18],
-    E: [-1,11,23]
-}
+  e: [-1, 11, 23],
+  B: [-8, 4, 16],
+  G: [-4, 8, 20],
+  D: [-11, 1, 13],
+  A: [-6, 6, 18],
+  E: [-1, 11, 23],
+};
 
 /*
 Intervals for "A" Minor Key
@@ -142,22 +132,14 @@ Intervals for "A" Minor Key
 5. perfect fifths
 6. minor sixths 
 7. minor sevenths 
-See "Relative Minor" concept
+- "Relative Minor" concept
 */
 
 const minorKeyRoots = sixths;
 const minorKeySeconds = sevenths;
 const minorKeyThirds = roots;
 const minorKeyFourths = seconds;
-const minorKeyFifths: Intervals = { 
-  // minorKeyFifths = fourths - 1
-    e: [0,12,24],  
-    B: [-19,-7,5,17],  
-    G: [-3,9,21], 
-    D: [-10,2,14,26],  
-    A: [-5,7,19],  
-    E: [0,12,24],  
-}
+const minorKeyFifths = thirds; 
 const minorKeySixths = fourths;
 const minorKeySevenths = fifths;
 
